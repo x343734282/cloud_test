f80e81ad0f99a22cfcbd98f24e7a0964a48258f2
john <xccjohn@gmail.com>
2018-08-07 15:55:37 +0800

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2018-08-07 15:50:53
+ * @version $Id$
+ */
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+	res.write('hello word');
+	res.end();
+});
+server.on('clientError', (err, socket) => {
+	socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+server.listen(8050);
