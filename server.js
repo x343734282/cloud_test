43eeb8f825b04bba1c38c02399fd25615e7db622
john <xccjohn@gmail.com>
2018-08-07 16:11:53 +0800

    init

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ const server = http.createServer((req, res) => {
 server.on('clientError', (err, socket) => {
 	socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
-server.listen(8050);
+server.listen(80);
